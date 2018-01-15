a2f998d3fe5dd5e1c93b2aaf33ef877619ede0cd
Eden Mugg <brillianteden@hotmail.com>
2018-01-15 02:27:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var playlist = { 'fakeArtist': 'notASong' };
 
 function updatePlaylist(playlist, artistName, songTitle) {
@@ -9,3 +10,6 @@ function removeFromPlaylist(playlist, artistName) {
   delete playlist.Kanye;
   return playlist;
 }
+=======
+var playlist = {};
+>>>>>>> 18a70c6d1dc0477e4c232d06fdfdf763fdeb2481
